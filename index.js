5aeff5b11fa40a42a1fcb2d1e4e6c722c4ef4bbc
RenanTyga <renan.rzprodutora@gmail.com>
2022-04-28 18:18:32 -0300

    teste

---
+++ index.js
@@ -0,0 +1,12 @@
+let herois = [
+    {
+        nome: "Superman",
+        poder: "Super Força"
+    },{
+        nome: "Batiman",
+        poder: "Inteligencia"
+    }
+
+
+]
+module.exports = herois;
